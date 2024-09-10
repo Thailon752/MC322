5dd553265f72657713f7c9e24a81956f906764ad
JaoRogeri <joaopedrorogeri@outlook.com>
2024-09-09 22:37:40 -0300

    mudei a main

--- Taxi-Lab2/src/Main.java
+++ Taxi-Lab2/src/Main.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
+import utils.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         //Aqui você deve realizar a simulação do funcionamento do sistema.
-      Passenger[]
+      Passenger[] passa = new Passenger[5];
+      Cabbie[] taxist = new Cabbie[5];
+      Vehicle[] carros = new Vehicle[5];
+      
+      for(int i =0;i<5;i++){
+        CabbieInfoGenerator a = new CabbieInfoGenerator();
+        Cabbie b = new Cabbie(a.getCabbieId(), a.getName(), a.getEmail(), a.getPhone(), a.getLicenseNumber());
+        taxist[i]=b;
+      }
         
     }
 
