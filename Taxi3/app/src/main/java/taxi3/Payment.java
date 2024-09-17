b47c6f4cbc34568df7f3f75679ece1be90224f0c
JaoRogeri <joaopedrorogeri@outlook.com>
2024-09-16 22:28:21 -0300

    aaaaaaa

--- Taxi3/app/src/main/java/taxi3/Payment.java
+++ Taxi3/app/src/main/java/taxi3/Payment.java
@@ -2,5 +2,11 @@ package taxi3;
 public interface Payment {
     
     // INTERFACE PAYMENT 
+    public abstract float getamont();
+    public abstract float getridedistance();
+    public abstract PaymentOption getoption();
+    public abstract String getpayid();
+    public abstract String getrideid();
+    public abstract void processPayment();
     
 }
--- Taxi3/app/src/main/java/taxi3/RidePayment.java
+++ Taxi3/app/src/main/java/taxi3/RidePayment.java
@@ -17,7 +17,14 @@ public class RidePayment implements Payment{
        
     // IMPLEMENTAR CONSTRUTUOR DA CLASS
 
-
+    private String noite_dia(LocalDateTime horario){
+        if(this.rideStartTime.getHour()<=19 && this.rideStartTime.getHour()<=5){
+            return "Diurno";
+        }
+        else{
+            return "Noturno";
+        }
+    }
 
     /**
      * Calculates the value of the ride.
@@ -26,35 +33,79 @@ public class RidePayment implements Payment{
      * 
      * @return the calculated value of the ride.
      */
-    public float calculateValue() {
-
-        switch (this.rideDistance) {
-            case this.rideDistance<=5:
-                
-                break;
-            case this.rideDistance<=10:
-
-                break;
-            case this.rideDistance<=15:
-                break;
-            case this.rideDistance<=20:
-
-                break;
-
-            case this.rideDistance>20:
-            
-            default:
-                break;
+    
+    private float valor_da_corrida() {
+        float valor =0;
+        
+        if(this.rideDistance <=5){
+            if(noite_dia(this.rideStartTime).equalsIgnoreCase("Diurno")){
+                valor =(float) (5 + 2*this.rideDistance);
+            }
+            else{
+                valor = (float)(6 + 2.5*this.rideDistance);
+            }
         }
+        else if(this.rideDistance<=10){
+            if(noite_dia(this.rideStartTime).equalsIgnoreCase("Diurno")){
+                valor =(float) (4 + 2.5*this.rideDistance);
+            }
+            else{
+                valor = (float)(5 + 3*this.rideDistance);
+            }
+        }
+        else if(this.rideDistance<=15){
+            if(noite_dia(this.rideStartTime).equalsIgnoreCase("Diurno")){
+                valor =(float) (3.5 + 3*this.rideDistance);
+            }
+            else{
+                valor = (float)(4.5 + 3.5*this.rideDistance);
+            }
+        }
+        else if(this.rideDistance<=20){
+            if(noite_dia(this.rideStartTime).equalsIgnoreCase("Diurno")){
+                valor =(float) (3 + 4*this.rideDistance);
+            }
+            else{
+                valor = (float)(4 + 4.5*this.rideDistance);
+            }
+        }
+        else if(this.rideDistance>20){
+            if(noite_dia(this.rideStartTime).equalsIgnoreCase("Diurno")){
+                valor =(float) (2.5 + 2*this.rideDistance);
+            }
+            else{
+                valor = (float)(3.5 + 2.5*this.rideDistance);
+            }
+        }
+        return valor;
+    }
+    private void calcula_value(){
+        this.amount = valor_da_corrida()+paymentMethod.gettaxa()*valor_da_corrida();
 
-        // IMPLEMENTAR METODO CALCULATE VALUE (CONSIDERANDO TAXA DO MÉTODO DE PAGAMENTO)
     }
 
-    
+    public float getamont(){
+        return this.amount;
+    }
+    public float getridedistance(){
+        return this.rideDistance;
+    }
+    public PaymentOption getoption(){
+        return this.paymentMethod;
+    }
+    public String getpayid(){
+        return this.paymentId;
+    }
+    public  String getrideid(){
+        return this.paymentId;
+    }
+
     /**
      * Processa o pagamento da corrida.
     */
+    @Override
     public void processPayment() {
+        
         // IMPLEMENTAR METODO PROCESS PAYMENT
     }
 
