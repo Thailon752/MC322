a07fbed4b0f69a041fb43b2821cb27e0a5c9755e
JaoRogeri <joaopedrorogeri@outlook.com>
2024-09-30 21:31:31 -0300

    a

--- Taxi3/app/build.gradle.kts
+++ Taxi3/app/build.gradle.kts
@@ -6,38 +6,31 @@
  */
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
     // Use JUnit Jupiter for testing.
-    testImplementation(libs.junit.jupiter)
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
-    implementation(libs.guava)
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    implementation("com.google.guava:guava:32.1.2-jre")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
 application {
-    // Define the main class for the application.
-    mainClass = "taxi3.App"
+    mainClass.set("taxi3.App")
 }
 
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
+// Explicitly define the JUnit Platform for tests
+tasks.test {
     useJUnitPlatform()
 }
